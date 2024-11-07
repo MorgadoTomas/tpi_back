db1df6642c8bd592cf367b9efa80d939a835f872
Tomy.m <morgadotomas24@gmail.com>
2024-11-07 18:43:33 -0300

    se cambio el nombre del archivo homeadmin

--- rutas/main.js
+++ rutas/main.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const usuariosRouter = require('./usuarios/usuarios');
-const adminRouter = require('./productos/homeadmin')
+const adminRouter = require('./productos/productos')
 const homeRouter = require('./productos/home')
 
 
