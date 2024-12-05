63e410bc527a146d1ebe90bb55fdfa72b3095ab5
joaquin <joaquinroman2006@gmail.com>
2024-12-05 03:34:02 -0300

    Cambio en index: Esto permite que cualquier archivo dentro de la carpeta public (incluidas las imágenes) sea accesible desde el navegador.

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const path = require('path');
 const apiRouter = require('./rutas/main');
 const cors = require('cors'); 
 const app = express();
-const port = 8080;
+const port = 4000;
 
 app.use(cors({ origin: 'http://localhost:5173' })); 
 app.use(express.json());
 app.use('/api', apiRouter);
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`Ejecutando servidor en el puerto ${port}`);
--- rutas/productos/productos.js
+++ rutas/productos/productos.js
@@ -1,4 +1,3 @@
-
 const router = require('express').Router();
 const { conexion } = require('../../conexion');
 const multer = require('multer');
