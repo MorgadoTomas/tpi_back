63e8128498589eaa96b339358d94e8ce46f40f32
joaquin <joaquinroman2006@gmail.com>
2024-12-12 14:17:33 -0300

    haciendo cambios minimos

--- rutas/verificarAdmin.js
+++ rutas/verificarAdmin.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const secret = '234u3i49kkfdsi8732934';
 const { conexion } = require('../conexion');
 
 const verificarAdmin = (req, res, next) => {
@@ -10,38 +9,23 @@ const verificarAdmin = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, secret);
-
-    // Solo para depuración, recuerda eliminar esto en producción
-    console.log('Datos decodificados del token:', decoded);
-    console.log("token es: ", token);
-
-    const usuario = decoded.usuario;  // El nombre de usuario del token
+    const decoded = jwt.verify(token, '234u3i49kkfdsi8732934');
+    const usuario = decoded.usuario;
     const query = 'SELECT admin FROM Usuarios WHERE usuario = ?';
 
     conexion.query(query, [usuario], (error, results) => {
       if (error) {
-        console.error('Error en la consulta:', error);
         return res.status(500).json({ message: 'Error en la base de datos' });
       }
 
-      if (results.length > 0) {
-        const isAdmin = results[0].admin;
-        const ADMIN_ROLE = 1;
-        const USER_ROLE = 0;
-
-        if (isAdmin === ADMIN_ROLE) {
-          return next(); // Si es admin, pasa al siguiente middleware
-        } else {
-          return res.status(403).json({ message: 'No tienes permisos de administrador' });
-        }
+      if (results.length > 0 && results[0].admin === 1) {
+        return next();
       } else {
-        return res.status(404).json({ message: 'Usuario no encontrado' });
+        return res.status(403).json({ message: 'No tienes permisos de administrador' });
       }
     });
 
   } catch (error) {
-    console.error('Error al verificar el token:', error);
     return res.status(401).json({ message: 'Token inválido o expirado. Por favor, inicia sesión nuevamente.' });
   }
 };
